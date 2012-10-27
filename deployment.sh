111b29a7b7610ff828c03157a525d7309cff22e1
Andrejs Buligins <buligin.developer@gmail.com>
2012-10-27 12:40:03 +0100

    Added __saved_data files beneath one-text modules to ignore list. Modified deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
 
-rsync -avrzt --copy-links --delete-during --progress --exclude 'error_log' --exclude '.git' -e ssh /Users/andrey/Sites/hanna/ andreybu@andreybuligin.com:public_html/rsync_test/
+rsync -avrzt --copy-links --delete-during --progress --exclude '__saved_data' --exclude 'error_log' --exclude '.git' -e ssh /Users/andrey/Sites/hanna/ andreybu@andreybuligin.com:public_html/hanna/
