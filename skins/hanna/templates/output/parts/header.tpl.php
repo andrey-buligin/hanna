b641af72673e69524aa87a3f059f7468241f82fe
Andrejs Buligins <buligin.developer@gmail.com>
2012-11-04 20:32:37 +0000

    Added new logo

--- skins/hanna/templates/output/parts/header.tpl.php
+++ skins/hanna/templates/output/parts/header.tpl.php
@@ -1,1 +1,6 @@
-<div id="logo"><a href="http://<?php echo $_CFG['path_url_full'].$web->lang_prefix;?>"><?php echo $this->getLogoImage();?></a></div>
+<div id="logo">
+	<a href="http://<?php echo $_CFG['path_url_full'].$web->lang_prefix;?>">
+	<div class="circle">&nbsp;</div>
+	Beauty <span class="hanna">by Hanna</span>
+	</a>
+</div>
