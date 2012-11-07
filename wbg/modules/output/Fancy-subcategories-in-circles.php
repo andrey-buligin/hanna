ba2b235ab085ff94b427273200617b088aa4bb59
Andrejs Buligins <buligin.developer@gmail.com>
2012-11-07 22:51:28 +0000

    Added new module for displaying subcategories in fancy animated circles

---
+++ wbg/modules/output/Fancy-subcategories-in-circles.php
@@ -0,0 +1,23 @@
+<article id="onetextContent">
+	<header>
+		<h1><?php echo WBG_HELPER::insertCatTitle() ?></h1>
+	</header>
+	<ul class="ch-grid">
+		<li class="ch-item">
+			<div class="ch-info"></div>
+			<div class="ch-info-front" style="background-image: url('images/images/text/220x_beautybyhannah.jpg')"></div>
+			<div class="ch-info-back">
+				<h3><a href="">Category title</a></h3>
+				<p>another description</p>
+			</div>
+		</li>
+		<li class="ch-item">
+			<div class="ch-info"></div>
+			<div class="ch-info-front" style="background-image: url('images/images/text/220x_beautybyhannah.jpg')"></div>
+			<div class="ch-info-back">
+				<h3><a href="">Category title</a></h3>
+				<p>another description</p>
+			</div>
+		</li>
+	</ul>
+</article>
