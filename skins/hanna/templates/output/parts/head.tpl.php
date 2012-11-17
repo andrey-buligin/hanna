0507a76573265bfdf0fcc99e31d6f8db2a05f7a4
Andrejs Buligins <buligin.developer@gmail.com>
2012-11-17 12:21:27 +0000

    Fixed viewport for mobiles

--- skins/hanna/templates/output/parts/head.tpl.php
+++ skins/hanna/templates/output/parts/head.tpl.php
@@ -6,8 +6,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width">
+    <!-- <meta name="viewport" content="width=device-width"> -->
 
 	<title><?php WBG::variable("page_title",1);?></title>
 	<meta name="description" content="<?php echo WBG_HELPER::generatePageMetaTag('desciption');?>" />
