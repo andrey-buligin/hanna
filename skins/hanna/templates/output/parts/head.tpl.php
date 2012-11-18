5a0f2e304168b6f9594871744e0e9ca256fe067b
Andrejs Buligins <buligin.developer@gmail.com>
2012-11-18 13:26:12 +0000

    Enabled GA for live env

--- skins/hanna/templates/output/parts/head.tpl.php
+++ skins/hanna/templates/output/parts/head.tpl.php
@@ -44,19 +44,19 @@
 
     <script type="text/javascript" src="http://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5095a7cf6d2d8dc2&amp;async=1"></script>
 
-	<!--<script type="text/javascript">
+    <?php global $_CFG; if ($_CFG['Environment'] == 'live') :?>
+		<script type="text/javascript">
+	      var _gaq = _gaq || [];
+	      _gaq.push(['_setAccount', 'UA-36089727-1']);
+	      _gaq.push(['_trackPageview']);
 
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-36089727-1']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
-    </script> -->
+	      (function() {
+	        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	      })();
+	    </script>
+    <?php endif; ?>
 
 </head>
 <?php WBG_HELPER::generatePageTitle();?>
